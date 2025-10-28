dee3ba79bd0294dc0d960cbec9ece8725c08f9ab
Giang Pham <giangpn25@gmail.com>
2025-10-27 23:11:31 -0500

    Track page access

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -71,10 +71,51 @@ sys_sleep(void)
 
 
 #ifdef LAB_PGTBL
+/*
+Few notes on user memory and kernel memory
+They are both memory but each process thinks that they have their own memory (virtual address) 
+All processes share the same kernel memory
+_> Page table ensures protection
+
+Protection is using flag like PTE_U: user can access
+*/
 int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 startingVA;
+  int num;
+  uint64 buffer;
+
+  argaddr(0, &startingVA);
+  argint(1, &num);
+  argaddr(2, &buffer);
+
+  struct proc *p = myproc();
+
+  int ans = 0;
+
+  for (int i = 0; i < num; i++){
+    uint64 va = startingVA + i*PGSIZE;
+
+    // the walk function gets the va and use the page table to get the correct page table entry (PTE)
+    // the PTE stores a set of bit including the physical page number with flags
+    // to get the physical address, use PTE2PA(pte)
+    pte_t *pte = walk(p->pagetable, va, 0);
+
+    if (*pte == 0) 
+      return -1;
+
+    if (*pte & PTE_A){
+      ans += (1 << i);
+      *pte &= ~PTE_A;
+    }
+  }
+
+  // copy from the kernel back to the user space
+  if (copyout(p->pagetable, buffer, (char*)&ans, sizeof(ans)) < 0)
+    return -1;
+  
   return 0;
 }
 #endif
